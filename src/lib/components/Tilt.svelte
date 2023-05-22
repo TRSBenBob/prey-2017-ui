94875307bfdcbf4261d09742b1e28d0658ee46f3
TRSBenBob <ben.rymill@gmail.com>
2023-05-22 11:33:31 +0100

    Initial UI explorations; Research screen

---
+++ src/lib/components/Tilt.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let amount = '15deg'
+
+  let tilt: HTMLDivElement
+
+  const handleMouseMove = (ev: MouseEvent) => {
+    const elem = ev.currentTarget as HTMLElement
+    const x = (ev.x - elem.offsetLeft - elem.offsetWidth / 2) / elem.offsetWidth
+    const y = (ev.y - elem.offsetTop - elem.offsetHeight / 2) / elem.offsetHeight
+
+    tilt.style.setProperty('--rx', x.toString())
+    tilt.style.setProperty('--ry', y.toString())
+  }
+
+  const handleMouseReset = (_ev: MouseEvent) => {
+    tilt.style.setProperty('--rx', '0')
+    tilt.style.setProperty('--ry', '0')
+  }
+</script>
+
+<div class="wrapper" on:mousemove={handleMouseMove} on:mouseleave={handleMouseReset}>
+  <div
+    bind:this={tilt}
+    style="--tilt-amount:{amount}"
+    class="tilt transition-transform duration-200 hover:duration-0"
+  >
+    <slot />
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    transform-style: preserve-3d;
+    transform: perspective(100vmax);
+  }
+
+  .tilt {
+    transform: rotateY(calc(var(--rx) * var(--tilt-amount, 15deg) * -1))
+      rotateX(calc(var(--ry) * var(--tilt-amount, 15deg)));
+  }
+</style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,47 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+import scrollbar from 'tailwind-scrollbar'
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#ffffff',
+        alto: '#d2d2d2',
+        'dusty-gray': '#999999',
+        'cutty-sark': '#799da9',
+        bismark: '#457080',
+        'cape-cod': '#35393a',
+        'ebony-clay': '#212b35',
+        'picton-blue': '#33c0ee',
+        'rob-roy': '#eeca6a',
+        roti: '#c8a954',
+        'coffee-bean': '#341213',
+        kilamanjaro: '#1c0402',
+        woodsmoke: '#090b0a',
+      },
+      fontFamily: {
+        sans: ['URW Gothic', ...fontFamily.sans],
+      },
+      dropShadow: ({ theme }) => ({
+        'coffee-bean': `-0.1875rem 0.0625rem 0 ${theme('colors.coffee-bean')}`,
+        roti: `-0.1875rem 0.0625rem 0 ${theme('colors.roti')}`,
+      }),
+      skew: {
+        15: '15deg',
+      },
+      boxShadow: ({ theme }) => ({
+        glow: `
+          0 0 .325rem ${theme('colors.picton-blue')},
+          0 0 .25rem ${theme('colors.white')}
+        `,
+      }),
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(circle at top, var(--tw-gradient-stops))',
+      },
+    },
   },
-  plugins: [],
+  plugins: [scrollbar({ nocompatible: true })],
 } satisfies Config
